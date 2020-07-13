8e78e08c7f2d0558ce4a9306b8c9f38bd9598d4c
Goi Jia Jian <jiajiannn@gmail.com>
2020-07-13 20:14:50 +0800

    feat: webhook

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const formsg = require("@opengovsg/formsg-sdk")()
+const request = require("request")
 
 const app = express()
 
-const formSecretKey = process.env.FORM_SECRET_KEY
-const googleApiKey = process.env.GOOGLE_API_KEY
-// Should match the URI supplied to FormSG in the form dashboard
-const POST_URI = process.env.POST_URI
+const FORM_SECRET_KEY = process.env.FORM_SECRET_KEY
+const POST_URI = process.env.POST_URI // Should match the URI supplied to FormSG in the form dashboard
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+
+const spreadsheetId = "1nhhD3XvHh2Ql_hW27LNw01fC-_I6Azt_XzYiYGhkmAU"
+const range = "'Form responses'!A:I"
 
 app.post(
   "/submissions",
@@ -32,7 +35,7 @@ app.post(
     // }
     /** @type {{responses: FormField[], verified?: Record<string, any>}} */
     const submission = formsg.crypto.decrypt(
-      formSecretKey,
+      FORM_SECRET_KEY,
       // If `verifiedContent` is provided in `req.body.data`, the return object
       // will include a verified key.
       req.body.data
@@ -41,11 +44,26 @@ app.post(
     // If the decryption failed, submission will be `null`.
     if (submission) {
       // Continue processing the submission
-      console.log(submission);
+      // console.log(submission);
 
       const documentStore = submission.responses[1].answer
-      console.log(documentStore);
-      
+      // can't use api key here...
+      request.post(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}:append?key=${GOOGLE_API_KEY}&valueInputOption=RAW&insertDataOption=OVERWRITE`, {
+        json: {
+          values: [
+            [
+              documentStore
+            ]
+          ]
+        }
+      }, (error, res, body) => {
+        if (error) {
+          console.error(error)
+          return
+        }
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(body)
+      })
     } else {
       // Could not decrypt the submission
     }
