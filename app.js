8de1108d0154ac10d0c2b7c8e6317698192449dc
Goi Jia Jian <jiajiannn@gmail.com>
2020-07-13 21:34:32 +0800

    feat: hook

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-const express = require("express")
-const formsg = require("@opengovsg/formsg-sdk")()
-const request = require("request")
+const express = require('express')
+const formsg = require('@opengovsg/formsg-sdk')()
+const request = require('request')
+var { google } = require('googleapis')
 
 const app = express()
+let sheets = google.sheets('v4');
 
 const FORM_SECRET_KEY = process.env.FORM_SECRET_KEY
 const POST_URI = process.env.POST_URI // Should match the URI supplied to FormSG in the form dashboard
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+const GOOGLE_SERVICE_ACCOUNT = require('./opencerts-service-account.json')
 
-const spreadsheetId = "1nhhD3XvHh2Ql_hW27LNw01fC-_I6Azt_XzYiYGhkmAU"
-const range = "'Form responses'!A:I"
+const spreadsheetId = '1nhhD3XvHh2Ql_hW27LNw01fC-_I6Azt_XzYiYGhkmAU'
+const range = '\'Form responses\'!A:I'
+
+const jwtClient = new google.auth.JWT(
+  GOOGLE_SERVICE_ACCOUNT.client_email,
+  null,
+  GOOGLE_SERVICE_ACCOUNT.private_key,
+  ['https://www.googleapis.com/auth/spreadsheets']
+);
+jwtClient.authorize(function (err, tokens) {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log("Successfully connected!");
+  }
+});
 
 app.post(
-  "/submissions",
+  '/submissions',
   // Endpoint authentication by verifying signatures
   function (req, res, next) {
     try {
-      formsg.webhooks.authenticate(req.get("X-FormSG-Signature"), POST_URI)
+      formsg.webhooks.authenticate(req.get('X-FormSG-Signature'), POST_URI)
       // Continue processing the POST body
       return next()
     } catch (e) {
-      return res.status(401).send({ message: "Unauthorized" })
+      return res.status(401).send({ message: 'Unauthorized' })
     }
   },
   // Parse JSON from raw request body after successful authentication
@@ -47,27 +64,31 @@ app.post(
       // console.log(submission);
 
       const documentStore = submission.responses[1].answer
-      // can't use api key here...
-      request.post(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}:append?key=${GOOGLE_API_KEY}&valueInputOption=RAW&insertDataOption=OVERWRITE`, {
-        json: {
+      console.log(documentStore)
+      const request = {
+        auth: jwtClient,
+        spreadsheetId: spreadsheetId,
+        range: range,
+        valueInputOption: 'RAW',
+        insertDataOption: 'OVERWRITE',
+        resource: {
           values: [
             [
               documentStore
             ]
           ]
         }
-      }, (error, res, body) => {
-        if (error) {
-          console.error(error)
-          return
-        }
-        console.log(`statusCode: ${res.statusCode}`)
-        console.log(body)
-      })
+      }
+      try {
+        var response = sheets.spreadsheets.values.append(request)
+        console.log(JSON.stringify(response, null, 2));
+      } catch (err) {
+        console.log(err);
+      }
     } else {
       // Could not decrypt the submission
     }
   }
 )
 
-app.listen(8080, () => console.log("Running on port 8080"))
+app.listen(8080, () => console.log('Running on port 8080'))
